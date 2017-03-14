2d54f6fed424d387b5e5902161d40b71bfa4076d
Mikel Duffy <mikelduffy1@gmail.com>
2017-03-14 02:50:07 -0700

    Implements header

---
+++ client/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <Jumbotron>
+      <h2>What does your number spell?</h2>
+      <p>Enter it below and find out!</p>
+    </Jumbotron>
+)};
+
+export default Header;
